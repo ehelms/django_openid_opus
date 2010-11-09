16f8b5a090aa3c2417a1b51faa77aeaa6d32b666
Eric D Helms <ericdhelms@gmail.com>
2010-11-08 22:07:46 -0500

    Added static media url to use with runserver and IF debug mode is on.

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 urlpatterns = patterns('django_openid_opus.views',
     url(r'^login/$', 'openid_login', name='openid_login_url'),
@@ -6,3 +7,10 @@ urlpatterns = patterns('django_openid_opus.views',
     url(r'^logout/$', 'logout_view', name='openid_logout_url'),
     url(r'^test/$', 'openid_test_page', name='openid_test_page_url'),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', 
+            { 'document_root' : settings.BASE_DIR + '/django_openid_opus/media/' }),
+    )
